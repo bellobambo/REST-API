7ed55e1bad28c0d00e2d22cead3b3676de9c9507
bellobambo <bellobambo21@gmail.com>
2023-10-27 22:01:56 +0100

    CORS

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const productsRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Headers", "PUT, POST, PATCH, DELETE, GET");
+    res.status(200).json({});
+  }
+  next();
+});
+
 
 app.use("/products", productsRoutes);
 app.use("/orders", orderRoutes);
@@ -17,12 +35,12 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message : error.message
-        }
-    })
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
 module.exports = app;
