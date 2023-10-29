6a2432e08c973b02f4d7483e0fa73166a183dbe5
bellobambo <bellobambo21@gmail.com>
2023-10-29 02:52:19 +0100

    orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,39 +1,68 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Order = require("../models/order");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "GET orders",
-  });
+  Order.find()
+    .select("product quantity _id")
+    .exec()
+    .then((docs) => {
+      res.status(200).json({
+        count: docs.length,
+        orders: docs.map((doc) => {
+          return {
+            id: doc._id,
+            product: doc.product,
+            quantity: doc.quantity,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/orders/" + doc._id,
+            },
+          };
+        }),
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.post("/", (req, res, next) => {
-    const order = {
-      productId: req.body.productId,
-      quantity: req.body.quantity,
-    };
-  res.status(201).json({
-    message: "Order created",
-    createdProduct: order,
+  const order = new Order({
+    _id: new mongoose.Types.ObjectId(),
+    quantity: req.body.quantity,
+    product: req.body.productId,
   });
+  order
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.get("/:orderId", (req, res, next) => {
-
-    const id = req.params.orderId;
-    if (id === "special") {
-      res.status(200).json({
-        message: "you discovered the special ID",
-        id: id,
-      });
-    } else {
-      res.status(200).json({
-        message: "you passed an ID",
-      });
-    }
-
+  const id = req.params.orderId;
+  if (id === "special") {
+    res.status(200).json({
+      message: "you discovered the special ID",
+      id: id,
+    });
+  } else {
+    res.status(200).json({
+      message: "you passed an ID",
+    });
+  }
 });
-
 
 router.delete("/:orderId", (req, res, next) => {
   res.status(200).json({
