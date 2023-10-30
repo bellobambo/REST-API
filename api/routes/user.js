6a895e5f25346e952932ca345f4abfdab3031103
bellobambo <bellobambo21@gmail.com>
2023-10-30 05:27:29 +0100

    jwt

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+
 
 const User = require("../models/user");
 
@@ -48,6 +51,52 @@ router.post("/signup", (req, res, next) => {
     .catch();
 });
 
+router.post("/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth Failed",
+        });
+      }
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth Failed",
+          });
+        }
+        if (result) {
+          const secret = process.env.JWT_KEY;
+          const token = jwt.sign(
+            {
+              email: user.email,
+              userId: user._id,
+            },
+            secret,
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({
+            message: "Auth Successful",
+            token: token,
+          });
+        }
+        res.status(401).json({
+          message: "Auth Failed",
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+
 router.delete("/:userId", (req, res, next) => {
   User.deleteOne({ _id: req.params.userId })
     .exec()
@@ -64,5 +113,8 @@ router.delete("/:userId", (req, res, next) => {
     });
 });
 
-
 module.exports = router;
+
+
+
+          // const secretkey ="eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5ODYzOTA2NiwiaWF0IjoxNjk4NjM5MDY2fQ.p3Yd0XiWRoLCEB4XtX-Ak6vFPRgYgudP-_08UVrj78w";
