08722b37ce1c18bcbebfeb7a41faa7d57661408e
Bambo Bello <user@Bambo.local>
2023-10-31 20:39:50 +0100

    new

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,42 +1,113 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+
+
+const User = require("../models/user");
+
+
 exports.user_sign_up = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-      .exec()
-      .then((user) => {
-        if (user) {
-          return res.status(409).json({
-            message: "email already exist",
-          });
-        } else {
-          bcrypt.hash(req.body.password, 10, (err, hash) => {
-            if (err) {
-              return res.status(500).json({
-                error: err,
-              });
-            } else {
-              const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash,
-              });
-  
-              user
-                .save()
-                .then((result) => {
-                  console.log(result);
-                  res.status(201).json({
-                    message: "User created successfully",
-                    user: result,
-                  });
-                })
-                .catch((err) => {
-                  console.log(err);
-                  res.status(500).json({
-                    error: err,
-                  });
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user) {
+        return res.status(409).json({
+          message: "email already exist",
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
+            });
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash,
+            });
+
+            user
+              .save()
+              .then((result) => {
+                console.log(result);
+                res.status(201).json({
+                  message: "User created successfully",
+                  user: result,
                 });
-            }
+              })
+              .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                  error: err,
+                });
+              });
+          }
+        });
+      }
+    })
+    .catch();
+}
+
+
+exports.user_login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth Failed",
+        });
+      }
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth Failed",
           });
         }
-      })
-      .catch();
-  }
+        if (result) {
+          const secret = process.env.JWT_KEY;
+          const token = jwt.sign(
+            {
+              email: user.email,
+              userId: user._id,
+            },
+            secret,
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({
+            message: "Auth Successful",
+            token: token,
+          });
+        }
+        res.status(401).json({
+          message: "Auth Failed",
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+}
+
+exports.user_delete = (req, res, next) => {
+  User.deleteOne({ _id: req.params.userId })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "Deleted successfully",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+}
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,79 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const checkAuth = require('../middleware/check-auth')
 
 
+const UserController = require("../controllers/user")
 
-const User = require("../models/user");
+router.post("/signup", UserController.user_sign_up );
 
-router.post("/signup", );
+router.post("/login",  UserController.user_login);
 
-router.post("/login", (req, res, next) => {
-  User.findOne({ email: req.body.email })
-    .exec()
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({
-          message: "Auth Failed",
-        });
-      }
-      bcrypt.compare(req.body.password, user.password, (err, result) => {
-        if (err) {
-          return res.status(401).json({
-            message: "Auth Failed",
-          });
-        }
-        if (result) {
-          const secret = process.env.JWT_KEY;
-          const token = jwt.sign(
-            {
-              email: user.email,
-              userId: user._id,
-            },
-            secret,
-            {
-              expiresIn: "1h",
-            }
-          );
-          return res.status(200).json({
-            message: "Auth Successful",
-            token: token,
-          });
-        }
-        res.status(401).json({
-          message: "Auth Failed",
-        });
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
-
-
-router.delete("/:userId", (req, res, next) => {
-  User.deleteOne({ _id: req.params.userId })
-    .exec()
-    .then((result) => {
-      res.status(200).json({
-        message: "Deleted successfully",
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
+router.delete("/:userId", checkAuth , UserController.user_delete );
 
 module.exports = router;
 
 
 
-          // const secretkey ="eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5ODYzOTA2NiwiaWF0IjoxNjk4NjM5MDY2fQ.p3Yd0XiWRoLCEB4XtX-Ak6vFPRgYgudP-_08UVrj78w";
